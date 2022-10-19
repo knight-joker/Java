58e1a1706e66adfaae757700941de2b797b38cce
knight <111353207+knight-joker@users.noreply.github.com>
2022-10-19 10:36:00 +0800

    Add files via upload

---
+++ NewStr.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class NewStr {
+
+	public static void main(String[] args) {
+		String str = "address";
+		String newstr = str.replace("a", "A");
+		System.out.println(newstr);
+		
+
+	}
+
+}
