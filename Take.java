5a35212ef2c437d518ec5bd3d3ccee5b6956c7f2
knight <111353207+knight-joker@users.noreply.github.com>
2022-11-10 11:21:12 +0800

    Add files via upload

---
+++ Take.java
@@ -0,0 +1,16 @@
+package day22;
+
+public class Take {
+	public static void main(String[] args) {
+		try {
+			String str = "lili";
+			System.out.println(str  + " 年龄是：");
+			int age = Integer.parseInt("20L");
+			System.out.println(age);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("program over");
+	}
+
+}
---
+++ cat.java
@@ -0,0 +1,7 @@
+package day22;
+
+public class cat extends animal {
+	public void shout() {
+		System.out.println("我是小猫我可以喵喵pp");
+	}
+}
