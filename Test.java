3280a510b61755302d638b9e8165c77b5dcdf3ef
knight <111353207+knight-joker@users.noreply.github.com>
2022-11-14 07:25:40 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,14 +1,17 @@
-package day11;
+package day26;
+
+import java.util.Scanner;
 
 public class Test {
-	//Somesentance
-	public void doSomething() {
-		//SomeSentance
+	//这是一个main方法，是程序的入口
+	public static void main(String args[]) {
+		//实现一个功能类：键输入两个数，求商
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入第一个数");
+		int num1 =  sc.nextInt();
+		System.out.println("请输入第二个数");
+		int num2 = sc.nextInt();
+		System.out.println(num1/num2);
 	}
-	protected Test dolt(){
-		return new Test();
-	}
-	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-	}
+
 }
--- Test2.java
+++ Test2.java
@@ -1,17 +1,26 @@
-package day11;
+package day26;
 
-  public class Test2 extends Test {
-	public Test2() {
-		super();
-		super.doSomething();
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Test2 {
+	public static void main(String args[]) {
+		//实现一个功能：通过键盘录入两个数，求商
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("请录入第一个数：");
+			int num1 = sc.nextInt();
+			System.out.println("请录入第二个数：");
+			int num2 = sc.nextInt();
+			System.out.println("商：" + num1/num2);
+		}catch(ArithmeticException e) {
+			System.out.println("对不起，除数不能为0");
+		}catch(InputMismatchException e) {
+			System.out.println("对不起，你录入的数据不是int类型的数据");
+		}finally {
+			System.out.println("-----谢谢你使用了计算机----");
+		}
+		
 	}
-	public void doSomethingnew(){
-		//SomeSentence
-	}
-	public void doSomething() {
-		//doSomeNewSentence
-	}
-	protected Test2 dolt() {
-		return new Test2();
-	}
+
 }
