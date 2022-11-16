9b82fb9850b89fdd5dd9a64e40e9bab694812ad2
knight <111353207+knight-joker@users.noreply.github.com>
2022-11-16 11:13:46 +0800

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,15 @@
+package Day28;
+
+
+public class Test1 {
+	public static void main(String args[]) {
+		//这是一个main方法，是程序的入口：
+		//属性
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		//物极必反
+		System.out.println(Integer.MAX_VALUE +1);
+		System.out.println(Integer.MIN_VALUE -1);
+	}
+
+}
--- Test2.java
+++ Test2.java
@@ -1,26 +1,9 @@
-package day26;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+package Day28;
 
 public class Test2 {
 	public static void main(String args[]) {
-		//实现一个功能：通过键盘录入两个数，求商
-		try {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("请录入第一个数：");
-			int num1 = sc.nextInt();
-			System.out.println("请录入第二个数：");
-			int num2 = sc.nextInt();
-			System.out.println("商：" + num1/num2);
-		}catch(ArithmeticException e) {
-			System.out.println("对不起，除数不能为0");
-		}catch(InputMismatchException e) {
-			System.out.println("对不起，你录入的数据不是int类型的数据");
-		}finally {
-			System.out.println("-----谢谢你使用了计算机----");
+		Integer i = 12;
+		System.out.println(i);
 		}
-		
-	}
 
 }
