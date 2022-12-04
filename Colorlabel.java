09b883b55571221748a76084828e988bfd6ed42a
knight <111353207+knight-joker@users.noreply.github.com>
2022-12-04 14:08:09 +0800

    Add files via upload

---
+++ Colorlabel.java
@@ -0,0 +1,19 @@
+package Day2;
+
+import java.awt.Color;
+
+import javax.swing.border.Border;
+
+public class Colorlabel {
+
+	public Colorlabel(String string, Color blue) {
+		// TODO 自动生成的构造函数存根
+		
+	}
+
+	public void setBorder(Border border1) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
--- MyFrame.java
+++ MyFrame.java
@@ -1,110 +1,33 @@
-package Day34;
+package Day2;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.image.ColorModel;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.text.DefaultCaret;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 public class MyFrame extends JFrame{
 	
-	public MyFrame(String title) {
+	public  MyFrame(String title) {
 		super(title);
 		
 		JPanel k = new JPanel();
 		this.setContentPane(k);
-		JButton b = new JButton("猪佩奇好棒");
-		MyControl c = new MyControl();
-		k.add(c);
-		k.add(b);
-		((Component) c).setPreferredSize(new Dimension(120,120));
 		
-		c.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				System.out.println("检测到鼠标点击");
-				System.out.println("点击位置" + e.getPoint());
-			
-				if(e.getButton() == MouseEvent.BUTTON1) {
-					System.out.println("点击的鼠标为左键");
-				}else if(e.getButton() == MouseEvent.BUTTON2) {
-					System.out.println("中键");
-				}else if(e.getButton() == MouseEvent.BUTTON3) {
-					System.out.println("右键");
-				}
-			}
-			
-		});
-		c.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO 自动生成的方法存根
-			
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				System.out.println("按钮被点击");
-				
-			}
-		});
-		b.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO 自动生成的方法存根
-				System.out.println("猪佩奇加油");
-			}
-		});
+		Colorlabel a1 = new Colorlabel("1",Color.BLUE);
+		Colorlabel a2 = new Colorlabel("1",Color.yellow);
+		
+		
+		Border border1 = new LineBorder(Color.gray,4);
+		a1.setBorder(border1);
+		
+		Border border2 = BorderFactory.createLineBorder(Color.red,4);
+		a1.setBorder(border2);
+		
 	}
+
 }
